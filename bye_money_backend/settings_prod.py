1e5bb0b59554912bdd73a6bc2ad7b06ed1289c0f
garciamendes <garciaprog0101@gmail.com>
2025-08-31 14:40:17 -0300

    first commit

---
+++ bye_money_backend/settings_prod.py
@@ -0,0 +1,14 @@
+import dj_database_url
+import os
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', ''),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
